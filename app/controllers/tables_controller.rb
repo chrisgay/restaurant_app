553bf5e3d5186b0b86949c037ba94b48eda10bdf
chrisgay <chris.gay@cmg2.co>
2013-10-28 16:07:20 -0500

    first commit

---
+++ app/controllers/tables_controller.rb
@@ -0,0 +1,9 @@
+class TablesController < ApplicationController
+
+  def index
+    @x = 1232 * 12
+  end  
+
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestaurantApp::Application.config.secret_key_base = '7503053981557487a2ad0d6c0c62f8ee374de033f433c0396b1070514cb65986d6ce97f10e449826d57fccc1fd01008fe2ac24dab95db6a438714d28dc4008a7'
